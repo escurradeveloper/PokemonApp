8be4c5487417a3c9151cf8e1409701e8946ca0a2
escurradeveloper <mescurrac123@gmail.com>
2025-01-29 19:33:38 -0500

    Create a new project with API Pokemon in IOS

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Escurra Colquis on 29/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
